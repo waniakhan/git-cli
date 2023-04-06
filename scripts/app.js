e4ca5152074239027767dd4a66d7cd66c3985fb4
waniakhan <MY_NAME@example.comgit>
2023-04-07 02:45:07 +0500

    FEAT:Initialize Project

---
+++ scripts/app.js
@@ -0,0 +1,63 @@
+
+// 3-april-2023
+
+// alert("hello world");
+// document.write("<h1>hello</h1>");
+
+// console.log(123);
+
+
+// var name = "ali";
+// var age = 24;
+// console.log(name);
+
+
+// alert("hello " +name + age);
+
+
+// let data = "wania";
+// let data2 = " khan";
+
+// let data3 = data + data2;
+// console.log(data3);
+
+// let data = 5;
+// let data2 = 5;
+
+// let data3 = data + data2; // addition
+// console.log(data3);
+
+
+// let userName = prompt("what is your name?")
+// // console.log(userName);
+// document.write("Welcome to my website "+userName);
+
+
+//  let randomNumer = Math.random();
+//  random = randomNumer *10
+//  console.log(randomNumer);
+
+
+// var 1product = 23;  (illegal);
+
+// var product1 = 45; (legal);
+
+// var nameOfProduct = "farm house"; (camel case)
+// var ProductMaterial = "fanulous"; (pascals case)
+// var product_material = "lattin"; (snake case)
+
+// data types 
+// 1. strings 
+// let name = "Wania";
+
+
+
+// 2. Number 
+//  let myLuckyNumber = 8;
+ // example = 4, 37, 0.37
+
+ // 3. Boolean
+// let isTodayMonday = true
+// example = true & false
+
+
