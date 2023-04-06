88c7722bb735e6b2b18a8c3490ca1b430103c55d
waniakhan <MY_NAME@example.comgit>
2023-04-07 02:57:00 +0500

    zakatpercentage

--- scripts/app.js
+++ scripts/app.js
@@ -60,4 +60,18 @@
 // let isTodayMonday = true
 // example = true & false
 
+  
+// 7-april-2023
+
+// var zakatPercentage = 0.025;
+ 
+// var userInput = prompt("User value");
+ 
+// //  userInput = parseInt("user value");
+// console.log(typeof Number(userInput));
+
+// var result = zakatPercentage * userInput;
+ 
+//  alert("Your zakat value is" + result);
+
 
